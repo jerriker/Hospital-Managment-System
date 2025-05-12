7476f64ce48d1ba7991e0955dfd61dd70f7cfb42
addison1234567 <addishailay@gmail.com>
2025-05-12 20:36:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
  * 2. Bekam Yoseph       ID: ETS0240/16
  * 3. Befiker Kassahun   ID: ETS0236/16
  * 4. Barok Yeshiber     ID: ETS0224/16
- * 5. Bethelhem Degefu   ID: ETS0283/16
- * 6. Addisalem Hailay   ID: ETS0100/16
+ * 5. Addisalem Hailay   ID: ETS0100/16
+ * 6. Bethelhem Degefu   ID: ETS0283/16
+ * 
 
  */
 #include<iostream>
